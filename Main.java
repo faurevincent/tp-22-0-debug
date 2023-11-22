61ed42920eb94dec76269fe3eaa3b32482d0b099
Faure <faurev@3il.fr>
2023-11-22 11:19:21 +0100

    Résolution des problèmes

--- Main.java
+++ Main.java
@@ -2,39 +2,47 @@ public class Main {
     public static void main(String[] args) {
         // Problème 1
         int[] numbers = {1, 2, 3, 4, 5};
-        for (int i = 0; i <= numbers.length; i++) {
+        for (int i = 0; i < numbers.length; i++) {
             System.out.println("Number at index " + i + ": " + numbers[i]);
         }
 
         // Problème 2
-        double result = calculateValue(5, 0);
-        System.out.println("Result: " + result);
+        try {
+        	int result = calculateValue(5, 0);
+            System.out.println("Result: " + result);
+        } catch (IllegalArgumentException e) {
+        	System.out.println(e.getMessage());
+        }
+ 
 
         // Problème 3
         int count = 10;
         while (count > 0) {
             System.out.println("Count: " + count);
-            count += 2;
+            count -= 2;
         }
 
         // Problème 4
         String message = "Bonjour";
         System.out.println("Longueur du message : " + message.length());
-        char character = message.charAt(10);
-        System.out.println("Caractère à l'indice 10 : " + character);
+        char character = message.charAt(message.length() - 1);
+        System.out.println("Caractère à l'indice 6 : " + character);
 
         // Problème 5
         String str1 = "Hello";
         String str2 = new String("Hello");
-        System.out.println("Comparaison de chaînes : " + (str1 == str2));
+        System.out.println("Comparaison de chaînes : " + (str1.equals(str2)));
 
         // Problème 6
-        Main mainObj = null;
+        Main mainObj = new Main();
         mainObj.method();
     }
 
     // Méthode pour le problème de division par zéro
     public static int calculateValue(int a, int b) {
+    	if (b == 0) {
+    		throw new IllegalArgumentException("Division par 0 !");
+    	}
         return a / b;
     }
 
